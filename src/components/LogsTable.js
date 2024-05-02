51b1e4fe6370043163045f62bde6609ccc9009e1
sun-i <111034439+sun-i@users.noreply.github.com>
2024-05-02 16:51:03 +0800

    fix bug

--- src/components/LogsTable.js
+++ src/components/LogsTable.js
@@ -610,10 +610,10 @@ const LogsTable = () => {
                 {process.env.REACT_APP_SHOW_BALANCE === "true" && (
                     <Space>
                         <Tag color="green" style={{ fontSize: '16px' }}>
-                            余额：${balance.toFixed(5)}
+                            余额：${balance}
                         </Tag>
                         <Tag color="blue" style={{ fontSize: '16px' }}>
-                            已用：${usage.toFixed(5)}
+                            已用：${usage}
                         </Tag>
                     </Space>
                 )}
